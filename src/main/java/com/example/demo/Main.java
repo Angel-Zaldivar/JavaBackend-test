de6b91bc97c206efd9e241a565fa0592bbaea676
Angel Zaldivar <142122005+Angel-Zaldivar@users.noreply.github.com>
2025-10-06 11:29:58 -0700

    Update Main.java

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,1 +1,18 @@
+package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@SpringBootApplication
+public class Main {
+	@RequestMapping("/json-example")
+	String jsonExample() {
+		return "{\"message\": \"Hello, World!\"}";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
